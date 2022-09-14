23a3d996af85adc29553480963ef9f321f7f889e
Ayush Kumar <ayushkaps9462@gmail.com>
2022-09-15 01:00:58 +0530

    Added main.rs file

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use::std::io::{
+    stdin,
+    prelude::*
+}
+use::std::env;
+use::hyper::{header, Body, Client, Request};
+use::hyper_tls::HttpsConnector;
+use::serde_deserve::{Serialize, Deserialize};
+use::spinners::{Spinner, Spinners}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
